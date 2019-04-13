f591de9655312e0ec1976738fb78ee4cfdb88222
Estel Seong <estel.castle@gmail.com>
2019-04-13 23:39:15 +0900

    Spring Data JPA - Auditing with CreatedBy, CreatedAt, LastModifiedBy, LastModifiedAt.

---
+++ src/main/java/com/example/cs/springbootcsmanagement/config/JpaAuditingConfig.java
@@ -0,0 +1,24 @@
+package com.example.cs.springbootcsmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+
+        // TODO: Security 연결 후 로그인한 계정 정보로 변경하기.
+        /*
+          if you are using spring security, you can get the currently logged username with following code segment.
+          SecurityContextHolder.getContext().getAuthentication().getName()
+         */
+        return () -> Optional.ofNullable("Anonymous");
+    }
+}
--- src/main/java/com/example/cs/springbootcsmanagement/domains/BaseEntity.java
+++ src/main/java/com/example/cs/springbootcsmanagement/domains/BaseEntity.java
@@ -8,16 +8,19 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @Getter
 @Setter
 @NoArgsConstructor
+@EntityListeners(value = {AuditingEntityListener.class})
 public abstract class BaseEntity {
 
     @CreatedBy
