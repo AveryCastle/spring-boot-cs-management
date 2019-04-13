8918156822f6aa28c63307885af60da429c40464
Estel Seong <estel.castle@gmail.com>
2019-04-13 20:37:03 +0900

    initial commit.

---
+++ src/main/java/com/example/cs/springbootcsmanagement/SpringBootCsManagementApplication.java
@@ -0,0 +1,12 @@
+package com.example.cs.springbootcsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCsManagementApplication.class, args);
+    }
+}
