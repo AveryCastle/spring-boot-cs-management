d59c2a55fc0ed6182ac9cce4c3f97d14c88584fb
Estel Seong <estel.castle@gmail.com>
2019-04-16 20:55:55 +0900

    Load PoC ~ QnAType 연결 데이터 for Local Environment.

--- src/main/java/com/example/cs/springbootcsmanagement/bootstrap/DataLoader.java
+++ src/main/java/com/example/cs/springbootcsmanagement/bootstrap/DataLoader.java
@@ -1,11 +1,14 @@
 package com.example.cs.springbootcsmanagement.bootstrap;
 
 import com.example.cs.springbootcsmanagement.domains.PoC;
+import com.example.cs.springbootcsmanagement.domains.PoCQnAType;
 import com.example.cs.springbootcsmanagement.domains.QnAType;
 import com.example.cs.springbootcsmanagement.enums.PoCType;
+import com.example.cs.springbootcsmanagement.repositories.PoCQnATypeRepository;
 import com.example.cs.springbootcsmanagement.repositories.PoCRepository;
 import com.example.cs.springbootcsmanagement.repositories.QnATypeRepository;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
@@ -25,26 +28,26 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     @Autowired
     private QnATypeRepository qnaTypeRepository;
 
+    @Autowired
+    private PoCQnATypeRepository pocQnATypeRepository;
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        if (pocRepository.count() == 0) {
-            log.debug("Loading PoCs.");
-
-            loadPocs();
-        }
-
-        if (qnaTypeRepository.count() == 0) {
-            log.debug("Loading QnATypes.");
-
-            loadQnATypes();
-        }
+        cleanDatas();
+        loadDatas();
     }
 
-    private void loadPocs() {
-        pocRepository.saveAll(Arrays.asList(new PoC(PoCType.OSC), new PoC(PoCType.OSB)));
+    private void cleanDatas() {
+        pocQnATypeRepository.deleteAll();
+        pocRepository.deleteAll();
+        qnaTypeRepository.deleteAll();
     }
 
-    private void loadQnATypes() {
+    private void loadDatas() {
+        PoC osc = new PoC(PoCType.OSC);
+        PoC osb = new PoC(PoCType.OSB);
+        pocRepository.saveAll(Arrays.asList(osc, osb));
+
         QnAType useTip = QnAType.builder().title("이용문의").expoOrder(3).build();
         QnAType purchase = QnAType.builder().title("구매/결제").expoOrder(2).build();
         QnAType device = QnAType.builder().title("단말").expoOrder(1).build();
@@ -61,5 +64,27 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
         QnAType deviceSupport = QnAType.builder().title("단말지원").expoOrder(1).upperQnAType(device).build();
         qnaTypeRepository.saveAll(Arrays.asList(deviceSupport));
+
+        PoCQnAType oscConnectionError = PoCQnAType.builder().poc(osc).qnaType(connectionError).expoOrder(1).build();
+        PoCQnAType oscAuthentication = PoCQnAType.builder().poc(osc).qnaType(authentication).expoOrder(2).build();
+        PoCQnAType oscMembership = PoCQnAType.builder().poc(osc).qnaType(membership).expoOrder(3).build();
+        pocQnATypeRepository.saveAll(Arrays.asList(oscConnectionError, oscAuthentication, oscMembership));
+
+        PoCQnAType oscPurchaseHistory = PoCQnAType.builder().poc(osc).qnaType(purchaseHistory).expoOrder(2).build();
+        PoCQnAType oscPurchaseCancel = PoCQnAType.builder().poc(osc).qnaType(purchaseCancel).expoOrder(1).build();
+        pocQnATypeRepository.saveAll(Arrays.asList(oscPurchaseHistory, oscPurchaseCancel));
+
+        PoCQnAType oscDeviceSupport = PoCQnAType.builder().poc(osc).qnaType(deviceSupport).expoOrder(1).build();
+        pocQnATypeRepository.save(oscDeviceSupport);
+
+        PoCQnAType osbConnectionError = PoCQnAType.builder().poc(osb).qnaType(connectionError).expoOrder(1).build();
+        PoCQnAType osbAuthentication = PoCQnAType.builder().poc(osb).qnaType(authentication).expoOrder(2).build();
+        pocQnATypeRepository.saveAll(Arrays.asList(osbConnectionError, osbAuthentication));
+
+        PoCQnAType osbPurchaseCancel = PoCQnAType.builder().poc(osb).qnaType(purchaseCancel).expoOrder(1).build();
+        pocQnATypeRepository.save(osbPurchaseCancel);
+
+        PoCQnAType osbDeviceSupport = PoCQnAType.builder().poc(osb).qnaType(deviceSupport).expoOrder(1).build();
+        pocQnATypeRepository.save(osbDeviceSupport);
     }
 }
