bc364b388e197c0a47fa81d20e68c51b458be115
Estel Seong <estel.castle@gmail.com>
2019-04-27 22:53:12 +0900

    Configure Multi-Module projects.

--- backend/src/test/java/com/example/cs/springbootcsmanagement/domains/QnATypeTest.java
+++ backend/src/test/java/com/example/cs/springbootcsmanagement/domains/QnATypeTest.java
@@ -1,7 +1,6 @@
 package com.example.cs.springbootcsmanagement.domains;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
